92ea8e6a7e12d28d5673f4151ed03ae5b561a826
unibank <ismayilova2606@gmail.com>
2022-06-06 15:22:56 +0400

    added style files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 
 
 router.get('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
